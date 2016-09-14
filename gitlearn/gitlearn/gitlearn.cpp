d5171697df9080f89b61a050252cf101d0688e01
CaryZhou <zyl940125@163.com>
2016-09-14 18:22:03 +0800

    new one

--- gitlearn/gitlearn/gitlearn.cpp
+++ gitlearn/gitlearn/gitlearn.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include<string>
 using namespace std;
+
 
 int main()
 {
+	string str = "heiheihei !!!";
 	cout << "Hello world!" << endl;
 	cout << "Hello github" << endl;
 	cout << "this is the change!!!" << endl;
+	cout << str << endl;
 	return 0;
 }
